913341ea2da85d8b452ad331aef2c76092f8f994
Dušan Uverić <dusan.uveric9@gmail.com>
2020-09-05 00:09:19 +0200

    fish: confd: bind forward/backward delete word
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -4,3 +4,8 @@ fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle plugin 'jethrokuan/z'
 
 fundle init
+
+# CTRL+Backspace
+bind \cH backward-kill-path-component
+# CTRL+Delete
+bind \e\[3\;5~ kill-word
