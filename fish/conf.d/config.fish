c004bc936321f5428e0758451602ea974b0d1a00
Dušan Uverić <dusan.uveric9@gmail.com>
2020-09-05 00:44:27 +0200

    fish: confd: add bin paths from home
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -6,6 +6,9 @@ fundle plugin 'jethrokuan/fzf'
 
 fundle init
 
+contains $HOME/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/bin
+contains $HOME/.cargo/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.cargo/bin
+
 # CTRL+Backspace
 bind \cH backward-kill-path-component
 # CTRL+Delete
