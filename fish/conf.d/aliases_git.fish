b0939bb7def662b4d7789c9337593ada548ac44e
Dušan Uverić <dusan.uveric9@gmail.com>
2020-11-26 15:54:35 +0100

    fish: confd: cleanup git aliases
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/aliases_git.fish
+++ fish/conf.d/aliases_git.fish
@@ -3,3 +3,40 @@ alias cond='git cherry-pick --continue'
 alias rsg='git reset'
 alias pick='git cherry-pick -s'
 alias skip='git cherry-pick --skip'
+
+alias g='git'
+
+alias ga='git add'
+alias gaa='git add --all'
+
+alias gap='git apply'
+
+alias gb='git branch'
+alias gba='git branch -a'
+alias gbd='git branch -d'
+
+alias gbs='git bisect'
+
+alias gc='git commit -v'
+alias gc!='git commit -v --amend'
+alias gcn!='git commit -v --no-edit --amend'
+alias gca='git commit -v -a'
+alias gca!='git commit -v -a --amend'
+alias gcan!='git commit -v -a --no-edit --amend'
+alias gcans!='git commit -v -a -s --no-edit --amend'
+alias gcam='git commit -a -m'
+alias gcsm='git commit -s -m'
+alias gcb='git checkout -b'
+alias gcf='git config --list'
+alias gcl='git clone --recurse-submodules'
+alias gclean='git clean -id'
+
+alias gsb='git status -sb'
+alias gsh='git show'
+alias gsi='git submodule init'
+alias gsps='git show --pretty=short --show-signature'
+alias gss='git status -s'
+alias gst='git status'
+
+alias gup='git pull --rebase'
+alias gupa='git pull --rebase --autostash'
