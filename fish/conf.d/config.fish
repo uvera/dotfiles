f4a56f38b8ef0b36eb10b9b97cc09e9db63f9c6e
Dušan Uverić <dusan.uveric9@gmail.com>
2021-01-22 23:02:34 +0100

    fish: confd: set insert mode blinking cursor to line
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -31,7 +31,7 @@ bind \e\cn '__ranger'
 
 fish_vi_key_bindings insert
 set fish_cursor_default block
-set fish_cursor_insert underscore
+set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 
