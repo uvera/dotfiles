3791574bca5a148188f2099d0a1a8fbcd3a85d0f
Dušan Uverić <dusan.uveric9@gmail.com>
2021-01-27 13:56:03 +0100

    firefox: add overrides
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

---
+++ firefox/user-overrides.js
@@ -0,0 +1,15 @@
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false); // see note above
+user_pref("privacy.clearOnShutdown.history", false); // Browsing & Download History
+user_pref("privacy.clearOnShutdown.offlineApps", false); // Offline Website Data
+user_pref("privacy.clearOnShutdown.sessions", false); // Active Logins
+
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+
+user_pref("keyword.enabled", true);
+
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+
+user_pref("browser.cache.disk.enable", true);
