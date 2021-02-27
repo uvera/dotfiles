112d51512e5a9f5af1056f9e7016d5b94ba602e1
Dušan Uverić <dusan.uveric9@gmail.com>
2021-02-27 17:14:08 +0100

    swap git aliases with abbreviations
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/aliases_git.fish
+++ fish/conf.d/aliases_git.fish
@@ -1,55 +1,55 @@
-alias logg='git log --pretty=short'
-alias cond='git cherry-pick --continue'
-alias rsg='git reset'
-alias pick='git cherry-pick -s'
-alias skip='git cherry-pick --skip'
+abbr -a -g logg git log --pretty=short
+abbr -a -g cond git cherry-pick --continue
+abbr -a -g rsg git reset
+abbr -a -g pick git cherry-pick -s
+abbr -a -g skip git cherry-pick --skip
 
-alias g='git'
+abbr -a -g g git
 
-alias ga='git add'
-alias gaa='git add --all'
+abbr -a -g ga git add
+abbr -a -g gaa git add --all
 
-alias gap='git apply'
+abbr -a -g gap git apply
 
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbd='git branch -d'
+abbr -a -g gb git branch
+abbr -a -g gba git branch -a
+abbr -a -g gbd git branch -d
 
-alias gbs='git bisect'
+abbr -a -g gbs git bisect
 
-alias gc='git commit -v'
-alias gcs='git commit -v -s'
-alias gc!='git commit -v --amend'
-alias gcn!='git commit -v --no-edit --amend'
-alias gca='git commit -v -a'
-alias gca!='git commit -v -a --amend'
-alias gcs!='git commit -s --no-edit --amend'
-alias gcan!='git commit -v -a --no-edit --amend'
-alias gcans!='git commit -v -a -s --no-edit --amend'
-alias gcam='git commit -a -m'
-alias gcsm='git commit -s -m'
-alias gcb='git checkout -b'
-alias gco='git checkout' 
-alias gcf='git config --list'
-alias gcl='git clone --recurse-submodules'
-alias gclean='git clean -id'
+abbr -a -g gc git commit -v
+abbr -a -g gcs git commit -v -s
+abbr -a -g gc! git commit -v --amend
+abbr -a -g gcn! git commit -v --no-edit --amend
+abbr -a -g gca git commit -v -a
+abbr -a -g gca! git commit -v -a --amend
+abbr -a -g gcs! git commit -s --no-edit --amend
+abbr -a -g gcan! git commit -v -a --no-edit --amend
+abbr -a -g gcans! git commit -v -a -s --no-edit --amend
+abbr -a -g gcam git commit -a -m
+abbr -a -g gcsm git commit -s -m
+abbr -a -g gcb git checkout -b
+abbr -a -g gco git checkout 
+abbr -a -g gcf git config --list
+abbr -a -g gcl git clone --recurse-submodules
+abbr -a -g gclean git clean -id
 
-alias gsb='git status -sb'
-alias gsh='git show'
-alias gsi='git submodule init'
-alias gsps='git show --pretty=short --show-signature'
-alias gss='git status -s'
-alias gst='git status'
+abbr -a -g gsb git status -sb
+abbr -a -g gsh git show
+abbr -a -g gsi git submodule init
+abbr -a -g gsps git show --pretty short --show-signature
+abbr -a -g gss git status -s
+abbr -a -g gst git status
 
-alias gup='git pull --rebase'
-alias gupa='git pull --rebase --autostash'
+abbr -a -g gup git pull --rebase
+abbr -a -g gupa git pull --rebase --autostash
 
-alias gp='git push'
-alias gpf='git push --force-with-lease'
-alias gpf!='git push --force'
+abbr -a -g gp git push
+abbr -a -g gpf git push --force-with-lease
+abbr -a -g gpf! git push --force
 
-alias gdc='git diff --cached'
-alias gdh='git diff HEAD'
+abbr -a -g gdc git diff --cached
+abbr -a -g gdh git diff HEAD
 
 alias gia='__git_abort'
 alias gcho='__git_checkout'
