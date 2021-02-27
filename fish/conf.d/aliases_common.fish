7265ff83fe5cd2c839524ba73016dd6d6f7980ae
Dušan Uverić <dusan.uveric9@gmail.com>
2021-02-27 17:14:23 +0100

    cleanup common aliases
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/conf.d/aliases_common.fish
+++ fish/conf.d/aliases_common.fish
@@ -1,25 +1,15 @@
-alias cls='clear;clear;clear'
-
-alias gus='gdrive upload --share'
-
 alias ls='__ls'
 alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
 
-alias nano='nvim'
-alias micro='nvim'
+abbr -a -g ydl youtube-dl -x --embed-thumbnail --add-metadata --audio-format mp3 -o "%(title)s.%(ext)s"
 
-alias ydl='youtube-dl -x --embed-thumbnail --add-metadata --audio-format mp3 -o "%(title)s.%(ext)s"'
-
-alias ftp_on='doas systemctl start pure-ftpd;doas systemctl start ddclient'
-alias ftp_off='doas systemctl stop pure-ftpd;doas systemctl stop ddclient'
-
-alias mb_bios='systemctl reboot --firmware-setup'
+abbr -a -g mb_bios systemctl reboot --firmware-setup
 
 alias icat="kitty +kitten icat"
 alias d="kitty +kitten diff"
 
 alias yayy="PKGEXT=.pkg.tar yay"
-alias pka="pikaur"
+abbr -a -g pka pikaur
