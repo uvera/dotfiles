0d203324fe62e4a53841297f6db9b1d9ba5f2686
Dušan Uverić <dusan.uveric9@gmail.com>
2021-05-14 21:51:34 +0200

    add rebase abort to git abort alias
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- fish/functions/__git_abort.fish
+++ fish/functions/__git_abort.fish
@@ -8,12 +8,15 @@ function __git_abort
             command git merge --abort
         case a
             command git am --abort
+	case b
+	    command git rebase --abort
         case '--help' or '-h'
             echo 'abort [OPTION]'
             echo 'c -- abort cherry-pick'
             echo 'r -- abort revert'
             echo 'm -- abort merge'
             echo 'a -- abort am'
+            echo 're -- abort rebase'
         case '*'
             echo "Abort $argv? What?"
     end
