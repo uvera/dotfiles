c8bd02a51dfbafa4da840ca3dc50da94000b898b
Dušan Uverić <dusanuveric@protonmail.com>
2021-06-20 17:14:54 +0200

    update doom emacs config
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,64 +1,57 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Dusan Uveric"
       user-mail-address "dusanuveric@protonmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-(setq +format-with-lsp nil)
-(setq web-mode-code-indent-offset 2)
 
 (setq doom-font (font-spec :family "Fira Code" :size 13 :weight 'medium))
 
-(+global-word-wrap-mode +1)
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type 'relative)
+
+;; enable word wrap globally
+(setq +global-word-wrap-mode 1)
+
+(setq web-mode-code-indent-offset 2)
+
+;; don't format with lsp
+(setq +format-with-lsp nil)
+
+;; scrolling tweaks
+(setq
+ ;; scroll N lines to screen edge
+ scroll-margin 4
+
+ ;; only 'jump' when moving this far off the screen
+ scroll-conservatively 100000
+
+ ;; ensure when we move outside the screen we always recenter
+ ;; (less hassle than attempting to make all jumping commands call recenter)
+ scroll-conservatively scroll-margin
+
+ ;; keyboard scroll one line at a time
+ scroll-step 1
+ ;; mouse scroll N lines
+ mouse-wheel-scroll-amount '(6 ((shift) . 1))
+ ;; don't accelerate scrolling
+ mouse-wheel-progressive-speed nil
+
+ ;; preserve line/column (nicer page up/down)
+ scroll-preserve-screen-position t
+ ;; Move the cursor to top/bottom even if the screen is viewing top/bottom (for page up/down)
+ scroll-error-top-bottom t
+
+ ;; center after going to the next error
+ next-error-recenter (quote (4))
+
+ ;; Always redraw immediately when scrolling,
+ ;; more responsive and doesn't hang!
+ ;; http://emacs.stackexchange.com/a/31427/2418
+ fast-but-imprecise-scrolling nil
+ jit-lock-defer-time 0
+ )
 
 (map! :leader (:when
                (featurep! :ui hydra)
