b442822ea066249c5e8e5f28584bbd76447d94fb
Dušan Uverić <dusanuveric@protonmail.com>
2021-08-16 17:16:08 +0200

    update fish config
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -15,6 +15,7 @@ contains $HOME/.cargo/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.ca
 contains $HOME/go/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/go/bin
 contains $HOME/.local/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.local/bin
 contains $HOME/.emacs.d/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.emacs.d/bin
+contains $HOME/.deno/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.deno/bin
 
 # CTRL+Backspace
 bind -M insert \cH backward-kill-path-component
