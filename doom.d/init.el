49fb21579e2129d12523bf529cc020176b8ccd92
Dušan Uverić <dusanuveric@protonmail.com>
2021-11-27 14:00:19 +0100

    doom emacs: update config
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- doom.d/init.el
+++ doom.d/init.el
@@ -97,9 +97,9 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        tmux              ; an API for interacting with tmux
